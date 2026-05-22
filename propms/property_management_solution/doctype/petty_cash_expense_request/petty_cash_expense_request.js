bc3ffe7187a36199dd09fd419c5b0d6f5cc73d78
Ibrahim Malinza <ibrahim@vvsdtz.com>
2026-05-22 15:12:19 +0300

    feat: Bump version to v15.0.30 and update Petty Cash Expense Request to include cost center field

--- propms/property_management_solution/doctype/petty_cash_expense_request/petty_cash_expense_request.js
+++ propms/property_management_solution/doctype/petty_cash_expense_request/petty_cash_expense_request.js
@@ -10,50 +10,47 @@ frappe.ui.form.on("Petty Cash Expense Request", {
 	},
 	refresh: function(frm) {
 		if (frm.doc.dr_account && frm.doc.cr_account) {
-			// Check if a non-submitted JE with this cheque_no already exists
 			frappe.db.get_value('Journal Entry', {
 				cheque_no: frm.doc.name,
-				docstatus: ["IN", [0, 1]]  // 0 = Draft, 1 = Submitted, 2 = Cancelled
+				docstatus: ["IN", [0, 1]]
 			}, 'name').then(r => {
-				if (r.message && r.message.name) {
-					// JE already exists, skip adding the button
-					return;
-				}
+				if (r.message && r.message.name) return;
 
 				frm.add_custom_button(__('Create Journal Entry'), function() {
-					frappe.route_options = {
-						"voucher_type": "Journal Entry",
-						// "company": frm.doc.company,
-						// "posting_date": frm.doc.posting_date,
-						"remarks": `Journal entry for Petty Cash Expense Request ${frm.doc.name}`
-					};
+					const cost_center = frm.doc.cost_center;
 
 					frappe.new_doc('Journal Entry', null, function(new_doc) {
 						new_doc.accounts = [];
+						new_doc.company = frappe.defaults.get_user_default("Company");
+						new_doc.voucher_type = "Cash Entry";
 						new_doc.posting_date = frm.doc.posting_date;
-						new_doc.company = frappe.defaults.get_user_default("Company")
-						
+						new_doc.user_remark = frm.doc.narration;
+						new_doc.cheque_no = frm.doc.name;
+						new_doc.cheque_date = frm.doc.posting_date;
+
 						let dr_row = frappe.model.add_child(new_doc, 'accounts');
 						dr_row.account = frm.doc.dr_account;
 						dr_row.debit_in_account_currency = frm.doc.amount_excl_vat;
 						dr_row.credit_in_account_currency = 0;
-						
+						dr_row.cost_center = cost_center;
+
 						if (frm.doc.vat_amount > 0) {
 							let vat_row = frappe.model.add_child(new_doc, 'accounts');
 							vat_row.account = frm.doc.tax_account;
 							vat_row.debit_in_account_currency = frm.doc.vat_amount;
 							vat_row.credit_in_account_currency = 0;
+							vat_row.cost_center = cost_center;
 						}
 
 						let cr_row = frappe.model.add_child(new_doc, 'accounts');
 						cr_row.account = frm.doc.cr_account;
 						cr_row.debit_in_account_currency = 0;
 						cr_row.credit_in_account_currency = frm.doc.amount;
+						cr_row.cost_center = cost_center;
 
-						new_doc.user_remark = frm.doc.narration;
-						new_doc.remark = frm.doc.narration;
-						new_doc.cheque_no = frm.doc.name;
-						new_doc.cheque_date = frm.doc.posting_date;
+						
+						// so the one-time JE refresh handler can pick it up
+						frappe._pending_cost_center = cost_center;
 
 						frappe.set_route('Form', 'Journal Entry', new_doc.name);
 					});
