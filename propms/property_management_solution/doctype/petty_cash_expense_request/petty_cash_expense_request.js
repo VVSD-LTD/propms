3363d8c9743c097ef5a4895dac89c2fa009ae819
Ibrahim Malinza <ibrahim@vvsdtz.com>
2026-07-22 17:02:59 +0300

    feat: Bump version to 15.0.37 and add journal entry tracking to Petty Cash Expense Request

--- propms/property_management_solution/doctype/petty_cash_expense_request/petty_cash_expense_request.js
+++ propms/property_management_solution/doctype/petty_cash_expense_request/petty_cash_expense_request.js
@@ -15,48 +15,77 @@ frappe.ui.form.on("Petty Cash Expense Request", {
 				docstatus: ["IN", [0, 1]]
 			}, 'name').then(r => {
 				if (r.message && r.message.name) return;
-
+				
 				frm.add_custom_button(__('Create Journal Entry'), function() {
 					const cost_center = frm.doc.cost_center;
+					// make confirmation dialog before creating journal entry
+					frappe.confirm(__('Are you sure you want to create a journal entry for this petty cash expense request?'), function() {
+						frappe.new_doc('Journal Entry', null, function(new_doc) {
+							new_doc.accounts = [];
+							new_doc.company = frappe.defaults.get_user_default("Company");
+							new_doc.voucher_type = "Cash Entry";
+							new_doc.posting_date = frm.doc.posting_date;
+							new_doc.user_remark = frm.doc.narration;
+							new_doc.cheque_no = frm.doc.name;
+							new_doc.cheque_date = frm.doc.posting_date;
+							new_doc.created_from_petty_cash_request = 1;
 
-					frappe.new_doc('Journal Entry', null, function(new_doc) {
-						new_doc.accounts = [];
-						new_doc.company = frappe.defaults.get_user_default("Company");
-						new_doc.voucher_type = "Cash Entry";
-						new_doc.posting_date = frm.doc.posting_date;
-						new_doc.user_remark = frm.doc.narration;
-						new_doc.cheque_no = frm.doc.name;
-						new_doc.cheque_date = frm.doc.posting_date;
-						new_doc.created_from_petty_cash_request = 1;
+							let dr_row = frappe.model.add_child(new_doc, 'accounts');
+							dr_row.account = frm.doc.dr_account;
+							dr_row.debit_in_account_currency = frm.doc.amount_excl_vat;
+							dr_row.credit_in_account_currency = 0;
+							dr_row.cost_center = cost_center;
 
-						let dr_row = frappe.model.add_child(new_doc, 'accounts');
-						dr_row.account = frm.doc.dr_account;
-						dr_row.debit_in_account_currency = frm.doc.amount_excl_vat;
-						dr_row.credit_in_account_currency = 0;
-						dr_row.cost_center = cost_center;
+							if (frm.doc.vat_amount > 0) {
+								let vat_row = frappe.model.add_child(new_doc, 'accounts');
+								vat_row.account = frm.doc.tax_account;
+								vat_row.debit_in_account_currency = frm.doc.vat_amount;
+								vat_row.credit_in_account_currency = 0;
+								vat_row.cost_center = cost_center;
+							}
 
-						if (frm.doc.vat_amount > 0) {
-							let vat_row = frappe.model.add_child(new_doc, 'accounts');
-							vat_row.account = frm.doc.tax_account;
-							vat_row.debit_in_account_currency = frm.doc.vat_amount;
-							vat_row.credit_in_account_currency = 0;
-							vat_row.cost_center = cost_center;
-						}
+							let cr_row = frappe.model.add_child(new_doc, 'accounts');
+							cr_row.account = frm.doc.cr_account;
+							cr_row.debit_in_account_currency = 0;
+							cr_row.credit_in_account_currency = frm.doc.amount;
+							cr_row.cost_center = cost_center;
 
-						let cr_row = frappe.model.add_child(new_doc, 'accounts');
-						cr_row.account = frm.doc.cr_account;
-						cr_row.debit_in_account_currency = 0;
-						cr_row.credit_in_account_currency = frm.doc.amount;
-						cr_row.cost_center = cost_center;
+							
+							// so the one-time JE refresh handler can pick it up
+							frappe._pending_cost_center = cost_center;
 
-						
-						// so the one-time JE refresh handler can pick it up
-						frappe._pending_cost_center = cost_center;
-
-						frappe.set_route('Form', 'Journal Entry', new_doc.name);
+							frappe.set_route('Form', 'Journal Entry', new_doc.name);
+						});
+						frappe.call({
+							method: "propms.property_management_solution.doctype.petty_cash_expense_request.petty_cash_expense_request.set_journal_entry_created",
+							args: {
+								petty_cash_expense_request: frm.doc.name
+							},
+							callback: function(r) {
+								if (r.message) {
+									frm.refresh_field("journal_entry_created");
+								}
+							}
+						});
 					});
-				});
+				})
 			});
+			
 		}
-	}
+		// if (frm.doc.docstatus === 1 && frm.doc.workflow_state === "Approved" && !frm.doc.journal_entry_created) {
+		// 	// Set journal_entry_created flag to true if a journal entry has been created for this petty cash expense request
+		// 	frappe.call({
+		// 		method: "propms.property_management_solution.doctype.petty_cash_expense_request.petty_cash_expense_request.check_if_journal_created",
+		// 		args: {
+		// 			petty_cash_expense_request: frm.doc.name
+		// 		},
+		// 		callback: function(r) {
+		// 			if (r.message) {
+		// 				frm.refresh_field("journal_entry_created");
+		// 			}
+		// 		}
+		// 	});
+		// }
+	},
+
 });
--- propms/property_management_solution/doctype/petty_cash_expense_request/petty_cash_expense_request.py
+++ propms/property_management_solution/doctype/petty_cash_expense_request/petty_cash_expense_request.py
@@ -7,3 +7,30 @@ from frappe.model.document import Document
 
 class PettyCashExpenseRequest(Document):
 	pass
+
+@frappe.whitelist()
+def check_if_journal_created(petty_cash_expense_request):
+	petty_cash_expense_request = frappe.get_doc("Petty Cash Expense Request", petty_cash_expense_request)
+	
+	journal_entry = frappe.db.get_value('Journal Entry', {
+		"cheque_no": petty_cash_expense_request.name,
+		"docstatus": ["IN", [0, 1]]
+	}, 'name')
+	if journal_entry:
+		if petty_cash_expense_request.journal_entry_created != 1:
+			petty_cash_expense_request.journal_entry_created = 1
+			petty_cash_expense_request.save()
+
+@frappe.whitelist()
+def set_journal_entry_created(petty_cash_expense_request):
+	petty_cash_expense_request = frappe.get_doc("Petty Cash Expense Request", petty_cash_expense_request)
+	petty_cash_expense_request.journal_entry_created = 1
+	petty_cash_expense_request.save()
+
+
+@frappe.whitelist()
+def patch_to_fix_journal_entry_created():
+	petty_cash_expense_requests = frappe.get_all("Petty Cash Expense Request", filters={"journal_entry_created": 0}, fields=["name"])
+	for request in petty_cash_expense_requests:
+		check_if_journal_created(request.name)
+	return "Patch executed successfully. Checked and updated journal_entry_created for all Petty Cash Expense Requests."
